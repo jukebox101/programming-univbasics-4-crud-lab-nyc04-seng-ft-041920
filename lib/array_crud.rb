a97c575e622f68137f4a2cb171290e65f2282382
Leonora Squires <leonora.squires@gmail.com>
2020-04-23 17:50:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,7 +24,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  retrieved_element = array(index_number)
+  retrieved_element
 end
 
 def retrieve_first_element_from_array(array)
