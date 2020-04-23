7a835a8e26eefcf4aaac270de256a7842a9ef622
Leonora Squires <leonora.squires@gmail.com>
2020-04-23 17:52:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -39,6 +39,5 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  array[index_number] = element
-  array[index_number]
+  p array[index_number] = element
 end
